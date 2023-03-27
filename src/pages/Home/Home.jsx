a1a4e066561733b9f8a95bddf22f162a2d8af44d
franktunde <adeekotobiloba8@gmail.com>
2023-03-27 15:31:52 +0100

    chore: Added redirect link to dahsboard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import './home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h3>Coming Soon...</h3>
+      <NavLink className="li" to='/dashboard'><p>Go to the Dashboard </p><MdOutlineKeyboardArrowRight style={{color:'white'}} className='icon'/></NavLink>
+    </div>
   )
 }
 
